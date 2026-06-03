35533b4f6a3d197c36ca4704fcfb94f036de30af
Ričards Cars <A250974RC@rvt.lv>
2026-06-03 15:08:05 +0300

    Update chess_functions.py

--- chess_functions.py
+++ chess_functions.py
@@ -1,1 +1,25 @@
+# Funkcija attēlo šaha dēli
 
+def paradit_deli(delis):
+    print()
+
+    for rinda in delis:
+        for lauks in rinda:
+            print(lauks, end=" ")
+        print()
+
+    print()
+
+
+# Funkcija pārvieto figūru
+
+def parvietot_figuru(delis, r1, c1, r2, c2):
+
+    # Saglabājam izvēlēto figūru
+    figura = delis[r1][c1]
+
+    # Pārvietojam figūru
+    delis[r2][c2] = figura
+
+    # Atbrīvojam veco vietu
+    delis[r1][c1] = " "
